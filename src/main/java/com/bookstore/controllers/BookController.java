f3f7a6f1c5232e06b42f531ee73dd5241be2d37e
lindson25 <lindson_lucas@live.com>
2024-05-21 12:25:32 -0300

    Adicionando o Swagger e anotações

--- src/main/java/com/bookstore/controllers/BookController.java
+++ src/main/java/com/bookstore/controllers/BookController.java
@@ -2,6 +2,13 @@ package com.bookstore.controllers;
 
 import com.bookstore.domain.Book;
 import com.bookstore.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,30 +17,66 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
 @RequiredArgsConstructor
+@RequestMapping("/books")
+@Tag(name = "Book Store", description = "Operações CRUD para gerenciamento de uma livraria")
 public class BookController {
+
     @Autowired
     private final BookService bookService;
 
+    @Operation(summary = "Criar um livro", description = "Cria um livro de acordo com os parâmetros enviados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Livro criado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class))),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos")
+    })
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookService.createBook(book);
+    public ResponseEntity<Book> createBook(
+            @Parameter(description = "Objeto do livro a ser criado", required = true, schema = @Schema(implementation = Book.class))
+            @RequestBody Book book) {
+        Book createdBook = bookService.createBook(book);
+        return ResponseEntity.ok(createdBook);
     }
 
+    @Operation(summary = "Retornar todos os livros", description = "Retorna todos os livros que estão no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Livros listados com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class))),
+    })
     @GetMapping
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
     }
 
+    @Operation(summary = "Atualizar um livro", description = "Atualiza um livro já existente através do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Livro atualizado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class))),
+            @ApiResponse(responseCode = "500", description = "Livro com o id não encontrado"),
+            @ApiResponse(responseCode = "404", description = "ID inválido")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+    public ResponseEntity<Book> updateBook(
+            @Parameter(description = "ID do livro a ser atualizado", required = true)
+            @PathVariable Long id,
+            @Parameter(description = "Objeto do livro com as novas informações", required = true, schema = @Schema(implementation = Book.class))
+            @RequestBody Book book) {
         Book updatedBook = bookService.updateBook(id, book);
         return ResponseEntity.ok(updatedBook);
     }
 
+    @Operation(summary = "Deletar um livro", description = "Deleta um livro através do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Livro deletado com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Livro não encontrado"),
+            @ApiResponse(responseCode = "404", description = "ID inválido")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+    public ResponseEntity<?> deleteBook(
+            @Parameter(description = "ID do livro a ser deletado", required = true)
+            @PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/bookstore/domain/Book.java
+++ src/main/java/com/bookstore/domain/Book.java
@@ -1,5 +1,6 @@
 package com.bookstore.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,14 +10,19 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Schema(description = "Detalhes sobre o livro")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID do livro", example = "1")
     private Long id;
 
     @Column(nullable = false)
+    @Schema(description = "Título do livro", example = "O Pequeno Príncipe")
     private String title;
 
+    @Schema(description = "Nome do autor", example = "Antoine de Saint-Exupéry")
     @Column(nullable = false)
     private String author;
 }
